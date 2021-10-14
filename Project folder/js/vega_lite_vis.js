11326d6f4d0759c90d5b609e8a7708a6594c9552
wpan0017 <56101902+AeroG81@users.noreply.github.com>
2021-10-15 02:09:27 +0800

    Initialise repo

---
+++ Project folder/js/vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "js/f1_geomap.vg.json";
+vegaEmbed("#F1map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "js/f1_constructor.vg.json";
+vegaEmbed("#F1constructor", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "js/f1_driver.vg.json";
+vegaEmbed("#F1driver", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "js/f1_constructor_champion.vg.json";
+vegaEmbed("#F1constructorchampion", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "js/f1_driver_champion.vg.json";
+vegaEmbed("#F1driverchampion", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
